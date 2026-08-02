f422763dfb9a16a8defa25f320b5f165846619d8
gauta <gauta@DESKTOP-5AO9SBN>
2026-08-02 16:48:41 +0530

    Add project files.

---
+++ Practice_for_Con/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Immutable;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.AspNetCore.Mvc;
+using Practice_for_Con.Models;
+
+namespace Practice_for_Con.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet("id")]
+        public IActionResult GetById(int id)
+        {
+            List<Product> products = new List<Product>()
+            {
+
+                    new Product { Id = 1, Name = "Laptop" },
+                    new Product { Id = 2, Name = "Mouse" }
+
+             };
+            List<int> ids = new List<int>();
+            foreach(var item in products)
+            {
+                ids.Add(item.Id);
+            }
+
+            if(!ids.Contains(id))
+            {
+                return BadRequest();
+            }
+            return Ok(products[id -1]);
+            
+        }
+    }
+}
---
+++ Practice_for_Con/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice_for_Con.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
