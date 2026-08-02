2fe8f2d6f713e3d4d559f06b44a3108f5db70513
Shubham Gautam <gautamShubham015@gmail.com>
2026-08-02 22:09:16 +0530

    Added search function

--- Practice_for_Con/Controllers/ProductsController.cs
+++ Practice_for_Con/Controllers/ProductsController.cs
@@ -10,7 +10,7 @@ namespace Practice_for_Con.Controllers
     public class ProductsController : ControllerBase
     {
 
-        [HttpGet("id")]
+        [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
             List<Product> products = new List<Product>()
@@ -23,12 +23,33 @@ namespace Practice_for_Con.Controllers
 
             var result = products.Find(x => x.Id == id);
 
-            if(result == null)
+            if (result == null)
             {
                 return BadRequest();
             }
-            return Ok(products[id -1]);
-            
+            return Ok(products[id - 1]);
+
+        }
+
+        [HttpGet("search")]
+        public ActionResult<Product> Search([FromQuery] string name)
+        {
+            List<Product> products = new List<Product>()
+            {
+
+                    new Product { Id = 1, Name = "Laptop" },
+                    new Product { Id = 2, Name = "Mouse" }
+
+             };
+
+            var product = products.Where(x => x.Name == name).FirstOrDefault(); 
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
         }
     }
 }
