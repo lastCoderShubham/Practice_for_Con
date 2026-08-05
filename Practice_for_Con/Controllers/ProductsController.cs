9b7bb0743b1856e38e665e7bc35d6ceb0cbc72c2
lastCoderShubham <102466464+lastCoderShubham@users.noreply.github.com>
2026-08-05 07:29:24 +0530

    Update ProductsController.cs
    v2

--- Practice_for_Con/Controllers/ProductsController.cs
+++ Practice_for_Con/Controllers/ProductsController.cs
@@ -63,30 +63,15 @@ namespace Practice_for_Con.Controllers
         }
     }
 }
-CREATE TABLE Employees
-(
-EmpID INT PRIMARY KEY,
-EmpName VARCHAR(50),
-Department VARCHAR(30),
-City VARCHAR(30),
-Salary INT,
-Experience INT
-);
-
-INSERT INTO Employees
-VALUES
-(101,'Amit','IT','Mumbai',50000,2),
-(102,'Neha','HR','Delhi',60000,4),
-(103,'Ravi','IT','Mumbai',70000,6),
-(104,'Priya','Finance','Pune',80000,8),
-(105,'Karan','HR','Delhi',55000,3),
-(106,'Rohan','Sales','Bangalore',65000,5),
-(107,'Sneha','IT','Pune',75000,7),
-(108,'Vikas','Finance','Mumbai',90000,10),
-(109,'Pooja','Sales','Delhi',62000,4),
-(110,'Rahul','IT','Bangalore',58000,3),
-(111,'Ankit','HR','Pune',53000,2),
-(112,'Meera','Finance','Delhi',85000,9),
-(113,'Kunal','Sales','Mumbai',67000,5),
-(114,'Simran','IT','Pune',72000,6),
-(115,'Arjun','HR','Mumbai',61000,4);
+SELECT COUNT(*) FROM Employees 
+SELECT SUM(Salary) as TotalSalary FROM Employees
+SELECT AVG(Salary) as AverageSalary from Employees
+Select MAX(Salary) as MaximumSalary from Employees
+Select MIN(Salary) AS LowestSalary FROM Employees
+SELECT Department, COUNT(*) FROM Employees GROUP BY Department;
+SELECT Department, AVG(Salary) FROM Employees GROUP BY Department;
+SELECT Department, AVG(Salary) FROM Employees GROUP BY Department HAVING AVG(Salary) > 70000
+SELECT Department, COUNT(Department) FROM Employees GROUP BY Department HAVING COUNT(*) > 3
+SELECT City, SUM(Salary) FROM Employees GROUP BY City
+SELECT Top 1 Department, AVG(Salary) AverageSalary FROM Employees GROUP BY Department ORDER BY AVG(Salary) DESC
+SELECT Top 1 City, COUNT(*) FROM Employees GROUP BY City ORDER BY COUNT(*) DESC
